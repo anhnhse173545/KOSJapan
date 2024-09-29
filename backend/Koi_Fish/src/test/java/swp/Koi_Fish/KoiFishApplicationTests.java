52cbd223b6f86540445a2bdfb623e32b29afb8e2
MinhKien17 <kiendmse170383@fpt.edu.vn>
2024-09-29 20:06:54 +0700

    Add basic structe for backend

---
+++ backend/Koi_Fish/src/test/java/swp/Koi_Fish/KoiFishApplicationTests.java
@@ -0,0 +1,13 @@
+package swp.Koi_Fish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoiFishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
