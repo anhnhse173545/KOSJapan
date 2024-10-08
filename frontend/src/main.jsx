76ea3373be805d7cc95eadf2663e3336e7a63dfc
NguyenHoangAnhSE173545 <anhnhse173545@fpt.edu.vn>
2024-10-08 09:32:21 +0700

    setup files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './styles/index.css'
+import { Homepage } from './components/src-components-homepage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Homepage />
   </StrictMode>,
 )
