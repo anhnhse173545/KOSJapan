854facf2cc5dc2b80c83544b784e6d08c1c5dfbd
minhnhse180474 <minhnhse180474@fpt.edu.vn>
2024-10-09 12:02:19 +0700

    header, footer

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Layout from "./components/layout";
+
+
+
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "",
+      element: <Layout/>,
+      children: [
+       
+      ],
+    },
+  ]);
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
