54396d58532fcdb85ec565fff8eb9418833a2cc0
minhnhse180474 <minhnhse180474@fpt.edu.vn>
2024-10-13 01:08:20 +0700

    fix

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -3,20 +3,7 @@ import './index.scss';
 
 const HomePage = () => {
   return (
-    <div className="homepage">
-      {/* Hero Section */}
-      <section className="hero">
-        <img
-          src="https://cf.ltkcdn.net/feng-shui/images/orig/291786-1600x1066-symbolism-koi-fish-feng-shui.jpg"
-          alt="Japanese Koi"
-          className="hero-image"
-        />
-        <div className="hero-text">
-          <h1>Koi Farm Trip: Txxxxxxxxxxx</h1>
-          <p>Discover the most beautiful Koi varieties to enhance your home or garden.</p>
-        </div>
-      </section>
-    </div>
+    <div>home</div>
   );
 }
 
