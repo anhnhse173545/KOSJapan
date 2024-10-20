a69fc8472de63d2fa93447b452773afe4d295947
ThanNguyen <thannthse183895@fpt.edu.vn>
2024-10-20 13:13:36 +0700

    Sửa bug fish order

--- backend/Koi_Ordering_System/src/main/java/com/swp391/koi_ordering_system/model/FishOrder.java
+++ backend/Koi_Ordering_System/src/main/java/com/swp391/koi_ordering_system/model/FishOrder.java
@@ -8,6 +8,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -37,8 +39,9 @@ public class FishOrder {
     private String deliveryAddress;
 
     @Column(name = "total")
-    private Double total;
+    private Double total = 0.0;
 
+    @CreationTimestamp
     @Column(name = "create_at")
     private LocalDateTime createAt;
 
