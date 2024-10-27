211dea6593b13a7823cd1d316291241a7ab1bb3e
ThanNguyen <thannthse183895@fpt.edu.vn>
2024-10-27 13:16:54 +0700

    gỡ authorize

--- backend/Koi_Ordering_System/src/main/java/com/swp391/koi_ordering_system/service/BookingService.java
+++ backend/Koi_Ordering_System/src/main/java/com/swp391/koi_ordering_system/service/BookingService.java
@@ -137,7 +137,9 @@ public class BookingService {
 
         boolean isCustomer = authService.isRole("ROLE_Customer");
 
-        boolean isManager = authService.isRole("ROLE_Manager");
+//        boolean isManager = authService.isRole("ROLE_Manager");
+
+        boolean isManager = true;
 
         boolean isSalesStaff = authService.isRole("ROLE_Sales_Staff");
 
